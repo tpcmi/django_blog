22fa27da586f33461d07453fcc702cd615b9f2d3
tpcmi <724801800@qq.com>
2022-01-02 23:50:39 +0800

    修改静态纹件地址

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
