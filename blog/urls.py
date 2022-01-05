9fc0c193c70e11904d484314349a52fd55d469c4
tpcmi <724801800@qq.com>
2022-01-05 12:46:48 +0800

    完善blog页面跳转

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list,name="post_list"), # ^（表示开头）并紧随 $（表示结尾），所以只有空字符串会被匹配到
-    re_path('post/(?P<pk>[0-9])+/$',views.post_detail,name="post_detail")
+    re_path(r'^post/(?P<pk>[0-9]+)/$',views.post_detail,name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,5 +12,6 @@ def post_list(request):
     return render(request, "blog/post_list.html", {"posts": posts})
 
 def post_detail(request,pk):
+    print('====pk========',pk)
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'post':post})
