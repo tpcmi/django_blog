ebf180df5a0344938999294df509f5e4a1661abb
tpcmi <724801800@qq.com>
2022-01-05 12:41:48 +0800

    完善blog页面跳转

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
     path('',views.post_list,name="post_list"), # ^（表示开头）并紧随 $（表示结尾），所以只有空字符串会被匹配到
+    re_path('post/(?P<pk>[0-9])+/$',views.post_detail,name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Post
 
@@ -10,3 +10,7 @@ def post_list(request):
         "published_date"
     )
     return render(request, "blog/post_list.html", {"posts": posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
