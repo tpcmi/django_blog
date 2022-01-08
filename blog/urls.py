5ef994e7356e7232fdd76f98d288b3b54ae55ac7
tpcmi <724801800@qq.com>
2022-01-08 11:40:39 +0800

    .

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',views.post_list,name="post_list"), # ^（表示开头）并紧随 $（表示结尾），所以只有空字符串会被匹配到
     re_path(r'^post/(?P<pk>[0-9]+)/$',views.post_detail,name="post_detail"),
     path('post/new/',views.post_new,name="post_new"),
-    re_path('post/(?P<pk>[0-9]+)/edit/',views.post_edit,name="post_edit")
+    re_path(r'post/(?P<pk>[0-9]+)/edit/$',views.post_edit,name="post_edit")
 ]
